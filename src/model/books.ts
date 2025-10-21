595402bbdec0d17bbfd8014922fbb39683c057d6
kithworths <mkithworthst@gmail.com>
2025-10-21 22:10:07 +0200

    Add Book and PaginatedResponse interfaces

--- src/model/books.ts
+++ src/model/books.ts
@@ -1,1 +1,15 @@
+export interface Book {
+  isbn: string;
+  title: string;
+  author: string;
+  publishedYear: number;
+  copies: number;
+}
 
+export interface PaginatedResponse<T> {
+  data: T[];
+  total: number;
+  page: number;
+  limit: number;
+  totalPages: number;
+}
